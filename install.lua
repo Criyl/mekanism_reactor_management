af6f81d736f96d4c21d04042cd823d8e556f3b7f
Chris Carroll <christopher.x.carroll@gmail.com>
2023-04-29 13:09:48 -0400

    chore: improve install script

--- install.lua
+++ install.lua
@@ -1,8 +1,14 @@
 local open = io.open
 
 function getFileURL(version, package)
-    return 'https://github.com/Criyl/'..'mekanism_reactor_management'..'/releases/download/'.. version ..'/'.. package
+    return 'https://github.com/Criyl/' .. 'mekanism_reactor_management' .. '/releases/download/' .. version .. '/' ..
+               package
 end
 
-local VERSION = '0.0.1'
+local VERSION = 'v0.0.1'
+local files = {'startup.lua', 'reactor_control.lua'}
 
+for i, file in pairs(files) do
+    print('installing ' .. file)
+    shell.run('wget ' .. getFileURL(VERSION, file) .. ' ' .. file)
+end
--- src/startup.lua
+++ src/startup.lua
@@ -2,11 +2,7 @@ local chatbox = peripheral.wrap('right')
 local reactor = peripheral.wrap('left')
 local monitor = peripheral.wrap('front')
 
-
-'https://github.com/Criyl/'..'mekanism_reactor_management'..'/releases/download/'.. VERSION ..'/'..''
-shell.run("pastebin get CmWgyMpn inventory")
-
-if pcall( shell.run('reactor_control.lua') ) then
+if pcall(shell.run('reactor_control.lua')) then
 else
     reactor.scram()
     reactorBroadcast('ERROR IN REACTOR CODE, REACTOR SHUTDOWN!')
